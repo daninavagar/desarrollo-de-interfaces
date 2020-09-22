2dd99e90c93198800cd9e5d597304af3b5002cfb
daninavagar <daninavagar@gmail.com>
2020-09-22 12:20:06 +0200

    4 y 5

---
+++ 1eva/src/Ejercicio4.java
@@ -0,0 +1,11 @@
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		/* 
+		 * Crear una matriz de enteros de n x n (tamaño lo decide el usuario), rellenar co números aleatorios, recorrerla y dar la suma de todos sus números.
+		 * */
+
+	}
+
+}
---
+++ 1eva/src/Ejercicio5.java
@@ -0,0 +1,11 @@
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		/*
+		 * Calcular el factorial de un número entero positivo a través de la recursividad.
+		 * */
+
+	}
+
+}
