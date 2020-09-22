7d80012a36066441021a3a725c6348df5304be72
daninavagar <daninavagar@gmail.com>
2020-09-22 12:11:16 +0200

    Ejercicio 2 y 3. Falta 3 por terminar

---
+++ 1eva/src/Ejercicio2.java
@@ -0,0 +1,23 @@
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+	/*
+	 * Crear un programa sencillo en el que se creen variable de tipo String, char, int, 
+	 * double, boolean. Asociarles un valor e imprimirlo por pantalla.
+	 * */	
+	String Nombre = "Daniel";
+	int numero = 10;
+	char letra = 'a';
+	double decimal = 13.3;
+	boolean exit = true;
+	
+	
+	
+	System.out.println("Hola: \n Nombre: "+Nombre+"\n Numero: "+numero+"\n Letra: "+letra+"\nDouble : "+decimal+"\nBoolean: "+exit);
+	
+	
+
+	}
+
+}
---
+++ 1eva/src/Ejercicio3.java
@@ -0,0 +1,22 @@
+
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+	/*
+	 * A partir de un array de enteros, recorrelo y obtener por pantalla la
+	 * cantidad total de números pares.
+	 * */
+
+	int i, cont = 0;	
+	
+	int[] intArray = new int[] {1,2,3,4,5};
+	
+	for ( i=0; i < intArray[i] ; i++){
+		cont++;
+		System.out.println(cont);
+		}
+		
+	}
+
+}
